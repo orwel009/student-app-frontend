1ffba89fbf1a4a6e4f7aae0a4ec925980ab5eb15
orwel009 <orwelvincent1@gmail.com>
2024-06-07 14:28:01 +0530

    Router Added

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const AddStudent = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Name</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">AdmissionNo</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">RollNo</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">ParentName</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">College</label>
+                            <textarea name="" id="" className="form-control"></textarea>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">DateOfBirth</label>
+                            <input type="date" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">EmailId</label>
+                            <input type="email" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">Password</label>
+                            <input type="password" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <label htmlFor="" className="form-label">ConfrimPassword</label>
+                            <input type="password" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                            <button className="btn btn-success">Register</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddStudent
---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,128 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const ViewAll = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">ORWEL P V</h5>
+                                    <p class="card-text">Brilliant Student with high problem solving mind</p>
+                                    <a href="#" class="btn btn-primary">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Rithwik Ramesh</h5>
+                                    <p class="card-text">Excellemt Student with high sceintific mind</p>
+                                    <a href="#" class="btn btn-primary">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Joe Sheran</h5>
+                                    <p class="card-text">Brilliant Student with high electronic skill</p>
+                                    <a href="#" class="btn btn-primary">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Meghana K H</h5>
+                                    <p class="card-text">Brilliant singer with efficient programming skills</p>
+                                    <a href="#" class="btn btn-primary">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Nivedhya K P</h5>
+                                    <p class="card-text">Dancer with efficient programming skills</p>
+                                    <a href="#" class="btn btn-primary">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">ORWEL P V</h5>
+                                    <p class="card-text">Brilliant Student with high problem solving mind</p>
+                                    <a href="#" class="btn btn-primary">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Rithwik Ramesh</h5>
+                                    <p class="card-text">Excellemt Student with high sceintific mind</p>
+                                    <a href="#" class="btn btn-primary">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Joe Sheran</h5>
+                                    <p class="card-text">Brilliant Student with high electronic skill</p>
+                                    <a href="#" class="btn btn-primary">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Meghana K H</h5>
+                                    <p class="card-text">Brilliant singer with efficient programming skills</p>
+                                    <a href="#" class="btn btn-primary">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Nivedhya K P</h5>
+                                    <p class="card-text">Dancer with efficient programming skills</p>
+                                    <a href="#" class="btn btn-primary">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Rithwik Ramesh</h5>
+                                    <p class="card-text">Excellemt Student with high sceintific mind</p>
+                                    <a href="#" class="btn btn-primary">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">ORWEL P V</h5>
+                                    <p class="card-text">Brilliant Student with high problem solving mind</p>
+                                    <a href="#" class="btn btn-primary">More Details</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ViewAll
