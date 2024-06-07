39b279293a7170c061b6ce0d9336b2cf4dd8ebe5
orwel009 <orwelvincent1@gmail.com>
2024-06-07 16:21:01 +0530

    Semi Dynamic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">StudentApp</a>
+                <Link class="navbar-brand" to="#">StudentApp</Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">AddStudent</a>
+                    <Link class="nav-link active" aria-current="page" to="/">AddStudent</Link>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="/search">SearchStudent</a>
+                    <Link class="nav-link" to="/search">SearchStudent</Link>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="/delete">DeleteStudent</a>
+                    <Link class="nav-link" to="/delete">DeleteStudent</Link>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="/view">ViewStudent</a>
+                    <Link class="nav-link" to="/view">ViewStudent</Link>
                     </li>
                 </ul>
                 </div>
--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const ViewAll = () => {
+
+    const [data,changeData] = useState(
+        [
+            {"Name":"ORWEL","AdmNo":111,"Roll":26 },
+            {"Name":"RICHI","AdmNo":121,"Roll":27 },
+            {"Name":"JOE","AdmNo":131,"Roll":28 },
+            {"Name":"RITHWIK","AdmNo":411,"Roll":29 },
+            {"Name":"MEGHNA","AdmNo":151,"Roll":36 },
+            {"Name":"SIVA","AdmNo":116,"Roll":37 },
+            {"Name":"NIVEDHYA","AdmNo":611,"Roll":38 },
+            {"Name":"ORWEL","AdmNo":111,"Roll":26 },
+            {"Name":"RICHI","AdmNo":121,"Roll":27 },
+            {"Name":"JOE","AdmNo":131,"Roll":28 },
+            {"Name":"RITHWIK","AdmNo":411,"Roll":29 },
+            {"Name":"MEGHNA","AdmNo":151,"Roll":36 },
+            {"Name":"SIVA","AdmNo":116,"Roll":37 },
+            {"Name":"NIVEDHYA","AdmNo":611,"Roll":38 }
+        ]
+    )
+
   return (
     <div>
         <Navbar/>
@@ -9,114 +29,24 @@ const ViewAll = () => {
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <div className="row g-3">
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">ORWEL P V</h5>
-                                    <p class="card-text">Brilliant Student with high problem solving mind</p>
-                                    <a href="#" class="btn btn-primary">More Details</a>
+
+                        {data.map(
+                            (value,index) => {
+                                return <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{value.Name}</h5>
+                                        <p class="card-text">{value.AdmNo}</p>
+                                        <p><b>{value.Roll}</b></p>
+                                        <a href="#" class="btn btn-primary">MoreDetails</a>
+                                    </div>
                                 </div>
+    
                             </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Rithwik Ramesh</h5>
-                                    <p class="card-text">Excellemt Student with high sceintific mind</p>
-                                    <a href="#" class="btn btn-primary">More Details</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Joe Sheran</h5>
-                                    <p class="card-text">Brilliant Student with high electronic skill</p>
-                                    <a href="#" class="btn btn-primary">More Details</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Meghana K H</h5>
-                                    <p class="card-text">Brilliant singer with efficient programming skills</p>
-                                    <a href="#" class="btn btn-primary">More Details</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Nivedhya K P</h5>
-                                    <p class="card-text">Dancer with efficient programming skills</p>
-                                    <a href="#" class="btn btn-primary">More Details</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">ORWEL P V</h5>
-                                    <p class="card-text">Brilliant Student with high problem solving mind</p>
-                                    <a href="#" class="btn btn-primary">More Details</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Rithwik Ramesh</h5>
-                                    <p class="card-text">Excellemt Student with high sceintific mind</p>
-                                    <a href="#" class="btn btn-primary">More Details</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Joe Sheran</h5>
-                                    <p class="card-text">Brilliant Student with high electronic skill</p>
-                                    <a href="#" class="btn btn-primary">More Details</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Meghana K H</h5>
-                                    <p class="card-text">Brilliant singer with efficient programming skills</p>
-                                    <a href="#" class="btn btn-primary">More Details</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Nivedhya K P</h5>
-                                    <p class="card-text">Dancer with efficient programming skills</p>
-                                    <a href="#" class="btn btn-primary">More Details</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">Rithwik Ramesh</h5>
-                                    <p class="card-text">Excellemt Student with high sceintific mind</p>
-                                    <a href="#" class="btn btn-primary">More Details</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title">ORWEL P V</h5>
-                                    <p class="card-text">Brilliant Student with high problem solving mind</p>
-                                    <a href="#" class="btn btn-primary">More Details</a>
-                                </div>
-                            </div>
-                        </div>
+                            }
+                        )}
+
                     </div>
                 </div>
             </div>
